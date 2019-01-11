62dca813c904f918bacd63eed4fefa2ffca17e44
“Kevin <logikevcoder@gmail.com>
2019-01-11 16:21:05 -0600

    refactored lodash code to chain functions using _.chain, added comments for future understanding of codebase

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,28 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+/*
+  Using Async await is possible because the action creators are being return as
+  functions due to using redux thunk. This gives more control over the functions
+  to return data in api requests in this scenario.
+  outer function is returning the inner function  () => async () =>
+*/
+
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   console.log('About to fetch posts');
   await dispatch(fetchPosts());
   // grabs array of all userId matches through lodash's map function that are unique
-  const userIds = _.uniq(_.map(getState().posts, 'userId'));
-  console.log(userIds)
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
   // call the fetch_user action creator for each id
-  userIds.forEach(id => dispatch(fetchUser(id))); // don't need the await keyword because all data has been retreived and there is nothing else to wait on after this
+  // userIds.forEach(id => dispatch(fetchUser(id))); 
+  // don't need the await keyword because all data has been retreived and there is nothing else to wait on after this
+
+  // does the same thing as the code above. 
+  _.chain(getState().posts)
+    .map('userId') // passes first arg (getState().posts) behind the scenes
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value(); // need this to execute the chained code
 };
 
 export const fetchPosts = () => async dispatch => { 
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import thunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
+// redux middle ware that injects thunk to return actions as functions to give more control using async await
 const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
