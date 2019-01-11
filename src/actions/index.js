5457d3d88e1994e6c0503647bb4101bbfd199e3e
“Kevin <logikevcoder@gmail.com>
2019-01-11 16:02:21 -0600

    refactored moization to make fetchpostsandusers action creator and retreive data in one place

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,16 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  console.log('About to fetch posts');
+  await dispatch(fetchPosts());
+  // grabs array of all userId matches through lodash's map function that are unique
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  console.log(userIds)
+  // call the fetch_user action creator for each id
+  userIds.forEach(id => dispatch(fetchUser(id))); // don't need the await keyword because all data has been retreived and there is nothing else to wait on after this
+};
+
 export const fetchPosts = () => async dispatch => { 
     const response = await jsonPlaceholder.get('/posts');
     
@@ -8,13 +18,8 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data});
   };
 
-export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
-
-  const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
-    dispatch({
-      type: 'FETCH_USER',
-      payload: response.data
-    });
-  });
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+  };
